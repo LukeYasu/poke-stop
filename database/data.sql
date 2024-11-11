d4bb3e1282262a29b6937fe590480dd14395907b
LukeYasu <lucasyasumura@gmail.com>
2024-11-11 17:56:54 +0000

    Initial commit

--- database/data.sql
+++ database/data.sql
@@ -9,3 +9,12 @@
 --      ('Learn to code', false),
 --      ('Build projects', false),
 --      ('Get a job', false);
+insert into "Items" ("name", "price", "photoUrl", "description", "quantity", "stock")
+values ('Pokeball', 100, 'pokeball.png', 'An essential part of any trainers bag. Helps you effectively catch pokémon.', 1, 1000),
+       ('Great Ball', 600, 'greatball.png', 'An upgraded, more effective version of the Pokéball that gives you an edge in performance when you are trying to catch em all.', 1,1000),
+       ('Ultra Ball', 1600, 'ultraball.png', 'the most refined Pokéball with the highest performance for any situation that is thrown your way.', 1, 1000),
+       ('Fire Stone', 2100, 'firestone.png', 'lets you evolve certain fire type Pokémon to give your partner a boost in their evolution and training journey.', 1, 1000),
+       ('Water Stone', 2100, 'waterstone.png', 'lets you evolve certain water type Pokémon to give your partner a boost in their evolution and training journey.', 1, 1000),
+       ('Leaf Stone', 2100, 'leafstone.png', 'lets you evolve certain grass type Pokémon to give your partner a boost in their evolution and training journey.', 1, 1000),
+       ('Dusk Stone', 2100, 'duskstone.png', 'lets you evolve certain dark type Pokémon to give your partner a boost in their evolution and training journey.', 1, 1000)
+returning *
--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,53 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "Items" (
+  "itemId" serial PRIMARY KEY,
+  "name" text,
+  "price" integer,
+  "photoUrl" text,
+  "description" text,
+  "quantity" integer,
+  "stock" integer,
+  "createdAt" timestamptz
+);
+
+CREATE TABLE "Users" (
+  "userId" serial PRIMARY KEY,
+  "username" varchar,
+  "hashedPassword" text,
+  "createdAt" timestamptz
+);
+
+CREATE TABLE "Cart" (
+  "cartId" serial PRIMARY KEY,
+  "userId" integer,
+  "itemId" integer,
+  "quantity" integer
+);
+
+CREATE TABLE "Order" (
+  "orderId" serial PRIMARY KEY,
+  "itemId" integer,
+  "userId" integer,
+  "quantity" integer
+);
+
+CREATE TABLE "Favorites" (
+  "favoriteId" serial PRIMARY KEY,
+  "itemId" integer,
+  "userId" integer
+);
+
+ALTER TABLE "Cart" ADD FOREIGN KEY ("userId") REFERENCES "Users" ("userId");
+
+ALTER TABLE "Order" ADD FOREIGN KEY ("itemId") REFERENCES "Items" ("itemId");
+
+ALTER TABLE "Cart" ADD FOREIGN KEY ("itemId") REFERENCES "Items" ("itemId");
+
+ALTER TABLE "Order" ADD FOREIGN KEY ("userId") REFERENCES "Users" ("userId");
+
+ALTER TABLE "Favorites" ADD FOREIGN KEY ("userId") REFERENCES "Users" ("userId");
+
+ALTER TABLE "Favorites" ADD FOREIGN KEY ("itemId") REFERENCES "Items" ("itemId");
