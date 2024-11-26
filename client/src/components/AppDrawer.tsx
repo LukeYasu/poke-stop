06af6be543f7e226091e8d85b9c32584f73720c6
LukeYasu <lucasyasumura@gmail.com>
2024-11-26 00:46:24 +0000

    minor changes

--- client/src/components/AppDrawer.tsx
+++ client/src/components/AppDrawer.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import { useUser } from './useUser';
-
+/* Is only visible on mobile view */
 export function AppDrawer() {
   const { toggleDrawer, drawerOpen } = useUser();
   return (
